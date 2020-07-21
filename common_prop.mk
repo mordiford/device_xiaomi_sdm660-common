f3519121ba6ce1761ed1c9b2bf9b64fd9f398a90
Max Weffers <rcstar6696@gmail.com>
2020-07-21 10:34:37 +0200

    sdm660-common: prop: Set Perf related Props from sdm660
    *Improves battery life alot
    
    This reverts commit 00fb44d6ef51591407bfd46960ea424245c5646f.
    
    Change-Id: I6837d8c3441955fa7dcb9537d595ad27b6f628d7

--- common_prop.mk
+++ common_prop.mk
@@ -249,13 +249,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
 
-# Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.qti.sys.fw.bg_apps_limit=36 \
-    ro.vendor.at_library=libqti-at.so \
-    vendor.perf.gestureflingboost.enable=true
-
 # Proximity
 PRODUCT_PROPERTY_OVERRIDES += \
     gsm.proximity.enable=true
@@ -266,7 +259,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
     persist.vendor.dpm.feature=1 \
     persist.vendor.dpm.nsrm.bkg.evt=3955 \
-    persist.vendor.qcomsysd.enabled=1
+    persist.vendor.qcomsysd.enabled=1 \
+    ro.vendor.extension_library=libqti-perfd-client.so
 
 # QTI
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
